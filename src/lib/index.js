1b15d14ecef93628f4e36d5b9a5884923a161fa6
alexjoeyyong <96444887+alexjoeyyong@users.noreply.github.com>
2023-11-30 17:47:49 +0000

    updates

--- src/lib/index.js
+++ src/lib/index.js
@@ -14,14 +14,13 @@ export default async function ({ token, delay, timeout }) {
   let timer = 0
 
   // init octokit
-  core.info(`${token}`);
   const octokit = github.getOctokit(token)
 
   // extract runId
   const { runId: run_id } = github.context
 
   // get workflow id and created date from run id
-  const { data: { workflow_id, run_started_at } } = await octokit.request('GET /repos/{owner}/{repo}/actions/runs/{run_id}', {
+  const { data: { workflow_id, run_started_at } } = await octokit.request(`GET /repos/optimizely/optimizely/actions/runs/${run_id}`, {
     ...github.context.repo,
     run_id
   })
